314d81e663d02714fdba0956795b0b05d90c3570
mashazyu <mariya.z@gmail.com>
2022-01-09 19:01:40 +0100

    Add closing icon to top menu

--- themes/quarteera/static/js/main.js
+++ themes/quarteera/static/js/main.js
@@ -1,9 +1,13 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
+  const openIcon = document.querySelector('.open-icon');
+  const closeIcon = document.querySelector('.close-icon');
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
+    openIcon.classList.toggle('hide');
+    closeIcon.classList.toggle('hide');
   })
 }
 
